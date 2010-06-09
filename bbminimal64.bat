79adb5be7498dfad2f776a48fb9181e43e89af61
karlh <karlh@2c67d6c2-4361-4cd7-bd83-1149b1926c9b>
2010-06-09 16:06:24 +0000

    x64 Windows boost libraries
    git-svn-id: https://valelab.ucsf.edu/svn/3rdpartypublic@9 2c67d6c2-4361-4cd7-bd83-1149b1926c9b

---
+++ bbminimal64.bat
@@ -0,0 +1,3 @@
+cd boost
+bjam --with-date_time --with-thread --stagedir=stage_x64 --build-dir=build_x64 link=static toolset=msvc-9.0 architecture=x86 address-model=64
+bjam --with-date_time --with-thread --with-system --with-regex --stagedir=stage_x64 --build-dir=build_x64 link=static runtime-link=static toolset=msvc-9.0 architecture=x86 address-model=64
